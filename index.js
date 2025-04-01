da1c0e3fea7117b19d6685548b5365435e4575ba
Yasmine Borghol <yesmine.borghol@gmail.com>
2025-04-01 12:38:50 +0200

    modified index js

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const activitiesRouter = require('./routes/activities');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Set view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
 app.get('/', (req, res) => {
   res.render('pages/home', { 
     title: 'Sweden Nature Reserves - Home',
@@ -22,11 +19,9 @@ app.get('/', (req, res) => {
   });
 });
 
-// Use routers
 app.use('/regions', regionsRouter);
 app.use('/activities', activitiesRouter);
 
-// 404 page
 app.use((req, res) => {
   res.status(404).render('pages/404', { 
     title: 'Page Not Found',
